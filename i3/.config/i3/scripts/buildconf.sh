caa1cfc899ad97adaf359f0ed95e9f58e4929f9f
JT Anderson <jtanderson@jtopia.org>
2021-08-27 22:19:31 -0700

    Fix race condition by moving i3 restart/reload commands into buildconf script

--- i3/.config/i3/scripts/buildconf.sh
+++ i3/.config/i3/scripts/buildconf.sh
@@ -13,3 +13,14 @@ for file in ${confs[@]};
 do
 	cat $file >> $cfgfile
 done
+
+if [ $1 == "restart" ];
+then
+  i3-msg restart
+elif [ $1 == "reload" ];
+then
+  i3-msg reload
+elif [ $1 == "exit" ];
+then
+  i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'
+fi
